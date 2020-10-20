d5fe4db1d561055a4b870606246c2c2b7dffcd55
Bashayr Alghamdi <bashayralghamdi@hotmail.com>
2020-10-21 00:43:17 +0300

    update code3

--- Hotels-Booking.R
+++ Hotels-Booking.R
@@ -6,6 +6,7 @@
 # load packages
 library(tidyverse)
 library(knitr)
+library(dplyr)
 
 # Get the Data
 hotels <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-11/hotels.csv')
@@ -17,8 +18,12 @@ glimpse(hotels)
 names(hotels)
 class(hotels)
 
+#remove NA value
+hotels_new <- hotels%>% 
+  na.omit() 
 
-#tidy data arrival date
+#tidy data 
+#arrival date
 #gather the date of arrival in one column
 hotels %>%
   unite(arrival_data,
@@ -34,7 +39,8 @@ glimpse(hotels_date)
 hotels %>% 
   select(stays_in_weekend_nights,stays_in_week_nights) %>% 
   mutate(stays_nights= stays_in_weekend_nights + stays_in_week_nights)->hotels_nights
-
+ 
+hotels_nights[23,]
 
 #Percentage of which month is there more reservation
 hotels %>% 
@@ -44,7 +50,6 @@ hotels %>%
 
 
 #Pie chart of parentage of all months
-#reference : https://www.youtube.com/watch?v=bzRD_nvvIVI 
 months_count
 pie <- ggplot(data=months_count,aes(x="",y=Percentage,fill=arrival_date_month)) +
   geom_col(color= "white")+
@@ -53,7 +58,16 @@ pie <- ggplot(data=months_count,aes(x="",y=Percentage,fill=arrival_date_month)) 
             position = position_stack(vjust=0.5))
 pie  
 
-
+hotels %>% 
+  mutate(kids = children+babies) %>% 
+  select(hotel,
+         is_canceled,
+         arrival_date_month,
+         adults,
+         kids,
+         required_car_parking_spaces)->hotel_ds
+hotel_ds$kids
+  
 #How mach family they are got reservation
 hotels %>% 
   select(reservation_status,adults, children,babies) %>% 
@@ -66,14 +80,42 @@ hotels_family %>%
 
 
 #How many customers take car parking space 
+
+
 hotels %>% 
   select(adults, babies, children,required_car_parking_spaces) %>% 
-  filter(babies+children == 0 & required_car_parking_spaces >= 1)#6,314 
+  filter(babies+children == 0 & required_car_parking_spaces >= 1) %>% 
+  summarise(count=n())#6,314 
+
+hotels %>% 
+  select(adults, babies, children,required_car_parking_spaces) %>% 
+  filter(babies+children == 0 & required_car_parking_spaces >= 1) %>% 
+  mutate(number_of_customer=adults+babies+children) %>% 
+  ggplot(aes(x = number_of_customer,y = required_car_parking_spaces))+
+  geom_jitter(alpha = 0.25)+
+  scale_y_continuous(limits = c(0,5),
+                     expand = c(0,0)) +
+  scale_x_continuous(limits = c(0,5),
+                     expand = c(0,0))
 
 #How many customers that had kids take car parking space 
 hotels %>% 
   select(adults, babies, children,required_car_parking_spaces) %>% 
-  filter(babies+children != 0 & required_car_parking_spaces >= 1) #1,102
+  filter(babies+children != 0 & required_car_parking_spaces >= 1) %>% 
+  summarise(count=n())#1,102
+
+hotels %>% 
+  select(adults, babies, children,required_car_parking_spaces) %>% 
+  filter(babies+children != 0 & required_car_parking_spaces >= 1) %>% 
+  mutate(number_of_customer=adults+babies+children) %>% 
+  ggplot(aes(x = number_of_customer,y = required_car_parking_spaces))+
+  geom_jitter(alpha = 0.25)+
+  scale_y_continuous(limits = c(0,3),
+                   expand = c(0,0)) +
+  scale_x_continuous(limits = c(1,6),
+                   expand = c(0,0)) 
+  
+  
 
 #what is type of customer that take car parking space 
 hotels %>% 
